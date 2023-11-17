192df20681d4fdeb20e624d58f8931011ff123ad
Andrea Cavallo <astarore@gmail.com>
2023-11-17 10:57:01 +0100

    Initial commit

---
+++ src/main/java/Exercises/E5_D15/E5D15Application.java
@@ -0,0 +1,13 @@
+package Exercises.E5_D15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E5D15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E5D15Application.class, args);
+	}
+
+}
